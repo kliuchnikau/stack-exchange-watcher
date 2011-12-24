8dc5de7b1b1cad5c70c1c0cf5e3256209a42e0f3
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@active.by>
2011-12-24 18:47:48 +0300

    Method to receive all users from SO

--- lib/stack_exchange/user_manager.rb
+++ lib/stack_exchange/user_manager.rb
@@ -5,7 +5,23 @@ module StackExchange
     end
 
     def find_from_country country, reputation = 0
+      result = []
+      page = 1
+      users = get_users(page)
 
+      result += users.users
+      total_users = users.total
+      while result.count < total_users
+        page += 1
+        result += get_users(page).users
+      end
+      result
+    end
+
+    private
+
+    def get_users(page)
+      @api.users.fetch({:pagesize => 100, :page => page})
     end
   end
 end
--- spec/stack_exchange/user_manager_spec.rb
+++ spec/stack_exchange/user_manager_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'ostruct'
 
 describe StackExchange::UserManager do
   let(:api_requestor) do
@@ -11,8 +12,19 @@ describe StackExchange::UserManager do
   describe '#find_from_country' do
     context 'without reputation filter' do
       it 'should query for all users on SO' do
-        api_requestor.should_receive(:users).with({:pagesize => 100}).and_return(nil)
-        user_manager.find_from_country 'Minsk'
+        users1 = mock('Users')
+        api_requestor.should_receive(:users).and_return(users1)
+        users1.should_receive(:fetch).with({:pagesize => 100, :page => 1})
+          .and_return(OpenStruct.new({'total' => 154, 'page' => 1, 'pagesize' => 100, 'users' => Array.new(100)}))
+
+        users2 = mock('Users')
+        api_requestor.should_receive(:users).and_return(users2)
+        users2.should_receive(:fetch).with({:pagesize => 100, :page => 2})
+          .and_return(OpenStruct.new({'total' => 154, 'page' => 2, 'pagesize' => 100, 'users' => Array.new(54)}))
+
+        users_list = user_manager.find_from_country 'Minsk'
+
+        users_list.should have(154).users
       end
 
       it 'should filter all SO user by specified country'
