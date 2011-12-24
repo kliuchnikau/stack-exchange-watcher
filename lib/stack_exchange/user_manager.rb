f44b7028c1019a5639e9cb4336b095ced091523b
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@active.by>
2011-12-24 20:52:54 +0300

    For users manager extracted constant and made one assert.

--- lib/stack_exchange/user_manager.rb
+++ lib/stack_exchange/user_manager.rb
@@ -1,5 +1,7 @@
 module StackExchange
   class UserManager
+    PAGE_SIZE = 100
+
     def initialize se_api_client
       @api = se_api_client
     end
@@ -14,6 +16,7 @@ module StackExchange
       result = users.users
       total_users = users.total
       while result.count < total_users
+        raise 'jumped over total users count' if page > (total_users / PAGE_SIZE)+1
         page += 1
         result += get_users(page, filter).users
       end
@@ -27,7 +30,7 @@ module StackExchange
     private
 
     def get_users(page, filter)
-      @api.users.fetch({:pagesize => 100, :page => page}.merge(filter) )
+      @api.users.fetch({:pagesize => PAGE_SIZE, :page => page}.merge(filter) )
     end
   end
 end
