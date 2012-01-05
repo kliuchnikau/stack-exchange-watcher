0a8c16e47a92a87944e7121020d5e85e1c0138ed
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2012-01-05 22:51:48 +0300

    Implemented check for new questions for specified tag

--- lib/stack_exchange/questions_manager.rb
+++ lib/stack_exchange/questions_manager.rb
@@ -6,7 +6,13 @@ module StackExchange
     end
 
     def get_new_questions tag
+      query_params = prepare_query_params(tag)
 
+      request_unixtime = Time.now.to_i
+      result_page = @requestor.questions.fetch(query_params)
+      set_last_tag_check(tag, request_unixtime)
+
+      result_page.questions
     end
 
     def set_last_tag_check tag, unixtime
@@ -15,6 +21,15 @@ module StackExchange
 
     def get_last_tag_check tag
       @tag_checks[tag]
+    end
+
+    private
+
+    def prepare_query_params(tag)
+      last_tag_check = get_last_tag_check tag
+      query_params = {:pagesize => 30, :page => 1, :sort => 'creation', :tagged => tag}
+      query_params[:fromdate] = last_tag_check if last_tag_check
+      query_params
     end
   end
 end
--- spec/stack_exchange/questions_manager_spec.rb
+++ spec/stack_exchange/questions_manager_spec.rb
@@ -3,30 +3,53 @@ require 'spec_helper'
 module StackExchange
   describe QuestionsManager do
     let(:requestor) { double('Rubyflow::Client').as_null_object }
-    before(:each) { @manager = StackExchange::QuestionsManager.new(requestor) }
+    let(:manager) { StackExchange::QuestionsManager.new(requestor) }
 
     describe '#set_last_tag_check' do
       it 'should set last tag check date' do
         unixtime = Time.now.to_i
-        @manager.set_last_tag_check 'ruby', unixtime
-        @manager.get_last_tag_check('ruby').should == unixtime
+        manager.set_last_tag_check 'ruby', unixtime
+        manager.get_last_tag_check('ruby').should == unixtime
       end
     end
 
     describe '#get_last_tag_check' do
       it 'should return nil if no time were set for this tag before' do
-        @manager.get_last_tag_check('c++').should be_nil
+        manager.get_last_tag_check('c++').should be_nil
       end
     end
 
     describe '#get_new_questions' do
-      it 'should receive new questions from requestor' do
-        fake_result = OpenStruct.new({'total' => 100500, 'page' => 1, 'pagesize' => 30, 'questions' => Array.new(30) })
+      def expect_requestor_call with_params
+        questions_received = Array.new(30)
+        fake_result = OpenStruct.new({'total' => 100500, 'page' => 1, 'pagesize' => 30, 'questions' => questions_received})
         questions = mock('Questions')
         requestor.should_receive(:questions).and_return(questions)
-        questions.should_receive(:fetch).with(pagesize: 30, page: 1, sort: 'creation', tagged: 'ruby').and_return(fake_result)  # fromdate: unixtime
+        questions.should_receive(:fetch).with(with_params).and_return(fake_result)
+        questions_received
+      end
 
-        @manager.get_new_questions('ruby')
+      it 'should receive new questions from requestor' do
+        questions_received = expect_requestor_call({:pagesize => 30, :page => 1, :sort => 'creation', :tagged => 'ruby' })
+
+        result = manager.get_new_questions('ruby')
+
+        result.should equal questions_received
+      end
+
+      it 'should set last tag check time after query for the tag' do
+        expect_requestor_call({:pagesize => 30, :page => 1, :sort => 'creation', :tagged => 'ruby' })
+
+        manager.should_receive(:set_last_tag_check).with('ruby', kind_of(Numeric))
+        manager.get_new_questions('ruby')
+      end
+
+      it 'should receive new questions from the last tag check time only' do
+        unixtime = Time.now.to_i
+        manager.set_last_tag_check('ruby', unixtime)
+        expect_requestor_call({:pagesize => 30, :page => 1, :sort => 'creation', :tagged => 'ruby', :fromdate => unixtime})
+
+        manager.get_new_questions('ruby')
       end
     end
   end
