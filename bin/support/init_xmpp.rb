7c6f8bfd36142ca4a772f6b3a5c45330684c470c
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2012-01-12 18:03:47 +0300

    renamed method to setup blatter

--- bin/support/init_xmpp.rb
+++ bin/support/init_xmpp.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 
-def get_xmpp_daemon
+def setup_blatter_xmpp_dsl
 	xmpp_config = YAML.load_file("#{CONFIG_FOLDER}/jabber.yml")
 	daemon_id = xmpp_config['daemon_jabber_id']
 	daemon_pass = xmpp_config['daemon_jabber_pass']
