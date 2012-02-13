db0e138746188c11be9ac0a910efcfa47c7cbc51
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2012-02-13 21:51:20 +0300

    Added test for logger

--- spec/watcher/questions_watcher_spec.rb
+++ spec/watcher/questions_watcher_spec.rb
@@ -14,11 +14,15 @@ describe Watcher::QuestionsWatcher do
   end
 
   describe '#check_tag' do
+    def fake_msg(creation_date)
+      OpenStruct.new(:tags => ['ruby', 'ruby-on-rails'], :creation_date => creation_date.to_i,
+                     :question_id => 123, :title => 'Question 1')
+    end
+
     context 'when connection with api.stackexchange and jabber is fine' do
       it 'should send message to view' do
         creation_date = Time.new(2012, 2, 12, 19, 52, 35)
-        questions = [OpenStruct.new(:tags => ['ruby', 'ruby-on-rails'], :creation_date => creation_date.to_i,
-                     :question_id => 123, :title => 'Question 1')]
+        questions = [fake_msg(creation_date)]
 
         qm.should_receive(:get_new_questions).with('ruby').and_return(questions)
         expected_msg = "\nhttp://www.stackoverflow.com/questions/123/ \"Question 1\" [ruby, ruby-on-rails] (19:52:35)"
@@ -39,6 +43,18 @@ describe Watcher::QuestionsWatcher do
         qm.should_receive(:get_new_questions).with(any_args).exactly(10).times.and_raise(::TimeoutError)
         view.should_receive(:send_msg_and_wait).with('Failed with TimeoutError', 61).once
         expect { subject.check_tag('ruby') }.to raise_error(::TimeoutError)
+      end
+    end
+
+    context 'when logger specified' do
+      it 'should log result received from questions manager to logger' do
+        logger = mock('logger')
+        subj =  Watcher::QuestionsWatcher.new(qm, view, logger)
+
+        qm.stub(:get_new_questions).and_return Array.new(3, fake_msg(Time.now))
+        logger.should_receive(:info).with("Found 3 results for 'ruby' tag")
+
+        subj.check_tag('ruby')
       end
     end
   end
